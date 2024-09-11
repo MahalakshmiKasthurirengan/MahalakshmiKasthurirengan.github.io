6e9fbec246a47bd60a8f792afc0a43375b43a2c0
Mahalakshmi K <167908001+MahalakshmiKasthurirengan@users.noreply.github.com>
2024-09-11 16:01:02 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,39 @@
+console.log("Hello");
+const x = 10;
+console.log(x);
+// x = 20; we cant modify the const variable
+let y = 10;
+y = "hi";
+console.log(y);
+// let variable can be change their value though it is different variable
+// alert(x);
+let number = 10;
+let string = "10";
+console.log(number + " " + string);
+// take it as string , string color white and different color means other datatypes
+console.log(y);
+let boolean = true;
+console.log(boolean);
+let nullvalue = null;
+console.log(nullvalue);
+let undefindedvariable = undefined;
+console.log(undefindedvariable);
+
+let datajs = {
+    name : 'maha',
+    phone: 9566834113,
+    alive: true
+}
+let datajson = {
+    name : 'maha',
+    phone: 9566834113,
+    alive: true
+}
+console.log(datajs);
+console.log(datajson);
+let dataarray = ["data1", "hi", datajs, datajson,false]
+console.log(dataarray[2]);
+let hi = "hi";
+let Hi = "Hi";
+console.log(hi);
+console.log(Hi);
